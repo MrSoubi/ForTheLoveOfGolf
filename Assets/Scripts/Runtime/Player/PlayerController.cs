b213d2e27acafa027390d7ee896dfeaf8345031b
MrSoubi <122730595+MrSoubi@users.noreply.github.com>
2024-05-27 11:49:11 +0200

    Squashed commit of the following:
    commit 2e2eb4a26ed32fe0781d1b749b16275bf9480ab5
    Author: MrSoubi <122730595+MrSoubi@users.noreply.github.com>
    Date:   Mon May 27 11:46:30 2024 +0200
    
        Feat: Fonctions SetEnvironnementEffect et UnsetEnvironnementEffect (pas d'effet pour le moment)
    
    commit cbf8eb78f1ddbf442b0766ea2a09bf6f128fc5bf
    Author: Tristan Poveda <povedatristan@gmail.com>
    Date:   Mon May 27 11:39:34 2024 +0200
    
        fix : maxSpeed
    
    commit 77208c10a7ad8ef3237dfbdc2d31b5a879e93aa8
    Merge: bbe372f a791d89
    Author: Tristan Poveda <povedatristan@gmail.com>
    Date:   Mon May 27 11:38:46 2024 +0200
    
        Merge branch 'BallController' of https://github.com/MrSoubi/ForTheLoveOfGolf into BallController
    
    commit bbe372fbb6795e9ead31f3d28be0c628c06c8d2a
    Author: Tristan Poveda <povedatristan@gmail.com>
    Date:   Mon May 27 11:38:40 2024 +0200
    
        feat : switch
    
    commit a791d8927567fb7f56313c81125fef281022b095
    Author: MrSoubi <122730595+MrSoubi@users.noreply.github.com>
    Date:   Mon May 27 11:33:49 2024 +0200
    
        Feat: Gestion des charges de tir
    
    commit 9337a4837cd6c664a69a7cf8ac557bff91ea253d
    Author: MrSoubi <122730595+MrSoubi@users.noreply.github.com>
    Date:   Mon May 27 10:51:24 2024 +0200
    
        Feat: Ajout de la fonction Shoot(), à travailler avec le GD
    
    commit 744b7c1285e57ce6e0793dc613691bd0074cf05b
    Author: MrSoubi <122730595+MrSoubi@users.noreply.github.com>
    Date:   Mon May 27 10:48:44 2024 +0200
    
        Fix: Modification de la vitesse max lors d'un boost
    
    commit 05e8d34decf2e05356ade6457a281d12c0096ca6
    Merge: fbb6f8b b8607ab
    Author: Tristan Poveda <povedatristan@gmail.com>
    Date:   Mon May 27 10:30:50 2024 +0200
    
        Merge branch 'BallController' of https://github.com/MrSoubi/ForTheLoveOfGolf into BallController
    
    commit fbb6f8b03fa7c27ea06d9eadd270c8cc8258c970
    Author: Tristan Poveda <povedatristan@gmail.com>
    Date:   Mon May 27 10:30:39 2024 +0200
    
        feat : Input Fonction
    
    commit b8607ab366169185c9f3d0bc66ab6a01fc911fb2
    Author: MrSoubi <122730595+MrSoubi@users.noreply.github.com>
    Date:   Mon May 27 10:22:14 2024 +0200
    
        Fix: Link des materials Opaque et Transparent sur le script du player
    
    commit 522ca09916c03d0fce5b06058e1c29316523e228
    Author: MrSoubi <122730595+MrSoubi@users.noreply.github.com>
    Date:   Mon May 27 10:15:42 2024 +0200
    
        Feat: Ajout de la fonction Teleport()
    
    commit 1c3cb0fe2e66627bce6486257deb5a03c4002ca7
    Merge: c9ceed9 6b5fc54
    Author: MrSoubi <122730595+MrSoubi@users.noreply.github.com>
    Date:   Mon May 27 10:09:24 2024 +0200
    
        Merge branch 'BallController' of https://github.com/MrSoubi/ForTheLoveOfGolf into BallController
    
    commit c9ceed90cbfc01eb4c82ffd1cbdd0de8b1927b8c
    Author: MrSoubi <122730595+MrSoubi@users.noreply.github.com>
    Date:   Mon May 27 10:09:15 2024 +0200
    
        Feat: Ajout des fonctions Bump() et BumpFixed()
    
    commit 6b5fc54b9bcc3bb4017958471678b2eb6f9a5750
    Merge: 7f93c45 8302c06
    Author: Tristan Poveda <povedatristan@gmail.com>
    Date:   Mon May 27 09:47:51 2024 +0200
    
        Merge branch 'BallController' of https://github.com/MrSoubi/ForTheLoveOfGolf into BallController
    
    commit 7f93c45b7b76bcfa7cbc4e17c891134d2c23ba0b
    Author: Tristan Poveda <povedatristan@gmail.com>
    Date:   Mon May 27 09:47:21 2024 +0200
    
        feat : OnValidate()
    
    commit 8302c0645f3d1edbe25feb52a06f1b21863cb708
    Author: MrSoubi <122730595+MrSoubi@users.noreply.github.com>
    Date:   Mon May 27 09:42:15 2024 +0200
    
        Feat: Ajout d'une fonction Bump et renommage de la fonction Boost
    
    commit 75e331d93e833b91bf3031cf73bb079a2ec5f5ef
    Author: MrSoubi <122730595+MrSoubi@users.noreply.github.com>
    Date:   Mon May 27 09:35:42 2024 +0200
    
        Feat: Ajout des fonctions MakePlayerTransparent() et MakePlayerOpaque()
    
    commit 63341cd6cc46c4c4d32d9906a4d7befe2cf53331
    Author: MrSoubi <122730595+MrSoubi@users.noreply.github.com>
    Date:   Mon May 27 09:29:32 2024 +0200
    
        Fix: Collision sur la Rolling Camera

--- Assets/Scripts/Runtime/Player/PlayerController.cs
+++ Assets/Scripts/Runtime/Player/PlayerController.cs
@@ -1,34 +1,60 @@
+using DG.Tweening;
 using System;
 using UnityEngine;
+using UnityEngine.UIElements;
+
+public enum EnvironmentEffect
+{
+    NORMAL,
+    ONWOOD,
+    MOIST,
+    CHILLED,
+    SLIMY
+}
 
 public class PlayerController : MonoBehaviour
 {
     [Header("Settings")]
     public float moveSpeed;
     public float maxSpeed;
+    public float airMultiplier;
     public float rotationSpeed;
     public float gravityForce;
 
-    [Header("References")]
-    private InputManager inputs;
+    private EnvironmentEffect environmentEffect = EnvironmentEffect.NORMAL;
+
     private Rigidbody rb;
 
-    public bool isGrounded;
+    private Vector3 direction;
+    private Vector3 gravity;
+    private Vector3 normal;
+    private Vector3 friction;
+    private Vector3 acceleration;
 
-    public Vector3 direction;
-    public Vector3 gravity;
-    public Vector3 normal;
-    public Vector3 friction;
-    public Vector3 acceleration;
+    private Vector2 playerInput;
+    private Vector2 mouseInput;
+
+    private bool isAiming;
+    private bool isGrounded;
+
+    // Transparence de la balle en mode Aim
+    public Material materialOpaque;
+    public Material materialTransparent;
+
+    private int shootCharges;
+    private int maxShootCharges;
 
     private void Awake()
     {
         rb = GetComponent<Rigidbody>();
-        inputs = GetComponent<InputManager>();
+
+        GetComponent<MeshRenderer>().material = materialOpaque;
     }
 
     private void Update()
     {
+        HandleInput();
+
         HandleDirection();
         HandleGravity();
 
@@ -46,21 +72,24 @@ public class PlayerController : MonoBehaviour
         HandleForces();
     }
 
+    private void HandleInput()
+    {
+        playerInput.x = Input.GetAxis("Horizontal");
+        playerInput.y = Input.GetAxis("Vertical");
+
+        mouseInput.x = Input.GetAxisRaw("Mouse X");
+        mouseInput.y = Input.GetAxisRaw("Mouse Y");
+    }
+
     private void HandleDirection()
     {
-        float inputMouse = Input.GetAxisRaw("Mouse X");
-        if (inputMouse != 0f)
-        {
-            float angle = Input.GetAxisRaw("Mouse X") * rotationSpeed;
-            rb.velocity = Quaternion.AngleAxis(angle, Vector3.up) * rb.velocity; // La direction tourne avec le mouvement de la souris
-        }
+        // La direction tourne avec le mouvement de la souris
+        rb.velocity = Quaternion.AngleAxis(mouseInput.x * rotationSpeed, Vector3.up) * rb.velocity; 
 
         if (rb.velocity.magnitude > 0.01)
         {
             direction = rb.velocity.normalized; // La direction de la balle est celle de la vélocité
         }
-
-        Debug.Assert(direction.magnitude > 0f);
     }
 
     private void HandleGravity()
@@ -82,21 +111,27 @@ public class PlayerController : MonoBehaviour
 
     private void HandleFriction()
     {
-        friction = Vector3.zero;
+        switch (environmentEffect)
+        {
+            case EnvironmentEffect.NORMAL:
+                friction = Vector3.zero;
+                break;
+        }
     }
 
     private void HandleAcceleration()
     {
-        acceleration = direction * Input.GetAxisRaw("Vertical") * moveSpeed * Time.deltaTime; // Avance
+        float accelerationSpeed = (isGrounded ? moveSpeed : moveSpeed * airMultiplier) * Time.deltaTime;
 
-        acceleration += Quaternion.AngleAxis(90, Vector3.up) * direction * moveSpeed * 100 * Input.GetAxisRaw("Horizontal") * Time.deltaTime; // Strafe
+        Vector3 verticalAcceleration = direction * playerInput.y * accelerationSpeed;
+        Vector3 horizontalAcceleration = Quaternion.AngleAxis(90, Vector3.up) * direction * accelerationSpeed * 100f * playerInput.x;
 
-        acceleration = Vector3.ClampMagnitude(acceleration, 5f);
+        acceleration = Vector3.ClampMagnitude(verticalAcceleration + horizontalAcceleration, 5f);
     }
 
     private void HandleForces()
     {
-        Vector3 forces = (gravity + normal + acceleration + friction);
+        Vector3 forces = gravity + normal + acceleration + friction;
 
         rb.AddForce(forces, ForceMode.Acceleration);
     }
@@ -105,7 +140,7 @@ public class PlayerController : MonoBehaviour
     private void CheckGround()
     {
         RaycastHit hit;
-        if(Physics.Raycast(transform.position, Vector3.down, out hit, transform.localScale.x * 0.5f + groundDetectionLength))
+        if (Physics.Raycast(transform.position, Vector3.down, out hit, transform.localScale.x * 0.5f + groundDetectionLength))
         {
             normal = hit.normal;
             isGrounded = true;
@@ -118,12 +153,105 @@ public class PlayerController : MonoBehaviour
 
     private void LimitSpeed()
     {
-        rb.velocity = Vector3.ClampMagnitude(rb.velocity, maxSpeed);
+        rb.velocity = Vector3.ClampMagnitude(rb.velocity, moveSpeed);
     }
 
-    public void BoostPlayer(Vector3 direction,float power)
+    /// <summary>
+    /// Ajoute un boost de vitesse à la balle dans la direction donnée en paramètre. Si la vitesse résultante est assez grande, elle devient la nouvelle vitesse max.
+    /// </summary>
+    /// <param name="direction"></param>
+    /// <param name="power"></param>
+    public void Boost(Vector3 direction,float power)
     {
-        rb.AddForce(direction * power * 5f, ForceMode.Impulse);
+        rb.AddForce(direction * power, ForceMode.Impulse);
+        
+        if (rb.velocity.magnitude > maxSpeed)
+        {
+            maxSpeed = rb.velocity.magnitude;
+        }
+    }
+
+    /// <summary>
+    /// Renvoie la balle en fonction du vecteur normal à la surface sur laquelle la balle est entrée en collision.
+    /// </summary>
+    /// <param name="normal"></param>
+    public void BumpFlipper(Vector3 normal)
+    {
+        float angle = Vector3.Angle(rb.velocity, normal);
+
+        rb.velocity = Quaternion.AngleAxis(angle * 2, normal) * rb.velocity;
+    }
+
+    /// <summary>
+    /// Projette la balle dans la direction donnée en paramètre, en gardant la force de la balle
+    /// </summary>
+    /// <param name="direction"></param>
+    public void BumpTrampoline(Vector3 direction)
+    {
+        rb.velocity = direction.normalized * rb.velocity.magnitude;
+    }
+
+    /// <summary>
+    /// Stop tous les mouvements de la balle et la téléporte à la position donnée en paramètre.
+    /// </summary>
+    /// <param name="newPosition"></param>
+    public void Teleport(Vector3 newPosition)
+    {
+        transform.position = newPosition;
+        rb.velocity = Vector3.zero;
+    }
+
+    private void MakePlayerOpaque()
+    {
+        GetComponent<MeshRenderer>().material.DOFade(1, 0.5f).OnComplete(() => { GetComponent<MeshRenderer>().material = materialOpaque; });
+    }
+
+    private void MakePlayerTransparent()
+    {
+        GetComponent<MeshRenderer>().material = materialTransparent;
+        GetComponent<MeshRenderer>().material.DOFade(0.2f, 0.5f);
+    }
+
+    /// <summary>
+    /// Informe la balle qu'elle vient de rentrer dans un environnement special
+    /// </summary>
+    /// <param name="effect"></param>
+    public void SetEnvironmentEffect(EnvironmentEffect effect)
+    {
+
+    }
+
+    /// <summary>
+    /// Informe la balle qu'elle vient de quitter un environnement special
+    /// </summary>
+    /// <param name="effect"></param>
+    public void UnsetEnvironmentEffect(EnvironmentEffect effect)
+    {
+
+    }
+
+    /// <summary>
+    /// Applique un effet de tir à la balle. S'applique uniquement si la balle dispose de charges de tir.
+    /// </summary>
+    /// <param name="direction"></param>
+    public void Shoot(Vector3 direction)
+    {
+        if (shootCharges > 0)
+        {
+            rb.AddForce(direction * 10f, ForceMode.Impulse);
+            shootCharges--;
+        }
+    }
+
+    /// <summary>
+    /// Ajoute des charges de tir à la balle. La fonction gère la quantité max de charges.
+    /// </summary>
+    /// <param name="amount"></param>
+    public void AddShootCharges(int amount)
+    {
+        shootCharges += amount;
+
+        Mathf.Clamp(shootCharges, 0, maxShootCharges);
     }
 
     private void OnDrawGizmos()

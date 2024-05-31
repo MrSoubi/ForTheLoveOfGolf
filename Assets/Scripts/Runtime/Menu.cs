03c27f52f6f5203ae76ab182a955b3375b476834
Furnok <killian.furno@orange.fr>
2024-05-31 14:51:18 +0200

    Fix: Correctifs

--- Assets/Scripts/Runtime/Menu.cs
+++ Assets/Scripts/Runtime/Menu.cs
@@ -3,19 +3,26 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.EventSystems;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class Menu : MonoBehaviour
 {
     public Texture2D handCursor;
 
-    public void CursorEnter()
+    public void CursorEnter(Button button)
     {
-        Cursor.SetCursor(handCursor, new Vector2(200, 0), CursorMode.Auto);
+        if(button.interactable)
+        {
+            Cursor.SetCursor(handCursor, new Vector2(200, 0), CursorMode.Auto);
+        }
     }
 
-    public void CursorExit()
+    public void CursorExit(Button button)
     {
-        Cursor.SetCursor(null, Vector2.zero, CursorMode.Auto);
+        if (button.interactable)
+        {
+            Cursor.SetCursor(null, Vector2.zero, CursorMode.Auto);
+        }
     }
 
     public void StartGame()

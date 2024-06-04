46ec6cfb89cd8b4f0b23a497f768e87608a9033c
Steamcollect <107922742+Steamcollect@users.noreply.github.com>
2024-06-04 13:45:29 +0200

    Feat : Camera manager, switch camera

--- Assets/Scripts/Runtime/Camera/Archives/CameraManager.cs
+++ Assets/Scripts/Runtime/Camera/Archives/CameraManager.cs
@@ -2,7 +2,6 @@ using Cinemachine;
 using UnityEngine;
 using System;
 
-
 public class CameraManager : MonoBehaviour
 {
     [Header("Initial Var")]
@@ -10,13 +9,21 @@ public class CameraManager : MonoBehaviour
     public CameraType Type {get {return cameraType;}}
 
     [Header("Virtuals Cameras References")]
-    [SerializeField] GameObject movementCam;
-    [SerializeField] GameObject lookAtCam;
-    
+    [SerializeField] CinemachineFreeLook followCam;
+    [SerializeField] CinemachineFreeLook aimCam;
+    [SerializeField] CinemachineBrain brain;
+
     public static CameraManager instance;
     void Awake()
     {
         instance = this;
+
+        // Set cameras target
+        Transform player = GameObject.FindGameObjectWithTag("Player").transform;
+        followCam.LookAt = player;
+        followCam.Follow = player;
+        aimCam.LookAt = player;
+        aimCam.Follow = player;
     }
 
     private void Start()
@@ -24,6 +31,17 @@ public class CameraManager : MonoBehaviour
         SetCameraType();
     }
 
+    // For testing
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.P)) SetType(CameraType.Aim);
+        if (Input.GetKeyDown(KeyCode.O)) SetType(CameraType.Follow);
+    }
+
+    /// <summary>
+    /// Mettre le camera type en parametre, change la camera actuel en le type voulu
+    /// </summary>
+    /// <param name="type"></param>
     public void SetType(CameraType type)
     {
         cameraType = type;
@@ -32,20 +50,127 @@ public class CameraManager : MonoBehaviour
 
     void SetCameraType()
     {
-        movementCam.SetActive(false);
-        lookAtCam.SetActive(false);
+        followCam.gameObject.SetActive(false);
+        aimCam.gameObject.SetActive(false);
 
         switch(cameraType)
         {
-            case CameraType.Movement:
-                movementCam.SetActive(true);
+            case CameraType.Follow:
+                followCam.gameObject.SetActive(true);
+                ToggleFollowMode();
                 break;
             
-            case CameraType.LooktAt:
-                lookAtCam.SetActive(true);
+            case CameraType.Aim:
+                aimCam.gameObject.SetActive(true);
+                ToggleAimMode();
                 break;
         }
     }
+
+    /// <summary>
+    /// Active la caméra de visée
+    /// </summary>
+    void ToggleAimMode()
+    {
+        aimCam.m_YAxis.Value = 0.5f;
+        aimCam.m_XAxis.Value = followCam.transform.rotation.eulerAngles.y;
+    }
+
+    /// <summary>
+    /// Active la caméra de suivi de la balle
+    /// </summary>
+    public void ToggleFollowMode()
+    {
+    }
+
+    /// <summary>
+    /// Renvoie la direction dans laquelle la caméra actuelle regarde
+    /// </summary>
+    /// <returns></returns>
+    public Vector3 GetShootingDirection()
+    {
+        Transform liveCamera = brain.ActiveVirtualCamera.VirtualCameraGameObject.transform;
+        Transform target = brain.ActiveVirtualCamera.LookAt;
+
+        Vector3 shootingDirection = target.position - liveCamera.position;
+
+        return shootingDirection;
+    }
 }
 
-public enum CameraType { Movement, LooktAt}
+public enum CameraType { Follow, Aim}
+
+/*private static CameraManager instance = null;
+    public static CameraManager Instance => instance;
+    private void Awake()
+    {
+        if (instance != null && instance != this)
+        {
+            Destroy(this.gameObject);
+            return;
+        }
+        else
+        {
+            instance = this;
+        }
+        DontDestroyOnLoad(this.gameObject);
+
+        // Initialisation du Game Manager...
+    }
+
+    [SerializeField] private CinemachineVirtualCamera rollingCam;
+    [SerializeField] private CinemachineFreeLook aimingCam;
+    private CinemachineBrain brain;
+
+    //bool isAiming = false;
+
+    void Start()
+    {
+        rollingCam.enabled = true;
+        aimingCam.enabled = true;
+
+        brain = GetComponentInChildren<CinemachineBrain>();
+    }
+
+    public void UseCamera(CinemachineVirtualCamera camera)
+    {
+
+    }
+
+    private void Update()
+    {
+        //Debug.Log(brain.transform.rotation.y * 180);
+    }
+    /// <summary>
+    /// Active la caméra de visée
+    /// </summary>
+    public void ToggleAimMode()
+    {
+        aimingCam.m_YAxis.Value = 0.5f;
+
+        aimingCam.m_XAxis.Value = rollingCam.transform.rotation.eulerAngles.y;
+
+        rollingCam.enabled = false;
+    }
+
+    /// <summary>
+    /// Active la caméra de suivi de la balle
+    /// </summary>
+    public void ToggleFollowMode()
+    {
+        rollingCam.enabled = true;
+    }
+
+    /// <summary>
+    /// Renvoie la direction dans laquelle la caméra actuelle regarde
+    /// </summary>
+    /// <returns></returns>
+    public Vector3 GetShootingDirection()
+    {
+        Transform liveCamera = brain.ActiveVirtualCamera.VirtualCameraGameObject.transform;
+        Transform target = brain.ActiveVirtualCamera.LookAt;
+
+        Vector3 shootingDirection = target.position - liveCamera.position;
+
+        return shootingDirection;
+    }*/

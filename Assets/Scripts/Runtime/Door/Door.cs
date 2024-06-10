38267e6a94b19164ec2712df40d7ebde02c1dc19
Furnok <killian.furno@orange.fr>
2024-06-10 11:39:55 +0200

    Fix: Correctifs

--- Assets/Scripts/Runtime/Door/Door.cs
+++ Assets/Scripts/Runtime/Door/Door.cs
@@ -29,6 +29,9 @@ public class Door : MonoBehaviour
     private int holeCompleted;
     private int coinCollected;
 
+    public float test1;
+    public float test2;
+
     private void Start()
     {
         StartCoroutine(Utils.Delay(LinkEvent, 0.001f));
@@ -108,7 +111,10 @@ public class Door : MonoBehaviour
     /// </summary>
     private void OpenPivot()
     {
-        doorLeft?.DORotate(new Vector3(transform.rotation.x, transform.rotation.y - 90, transform.rotation.z), animeDuration);
-        doorRight?.DORotate(new Vector3(transform.rotation.x, transform.rotation.y + 90, transform.rotation.z), animeDuration);
+        //doorLeft?.DORotate(new Vector3(transform.rotation.x, transform.rotation.y + test1, transform.rotation.z), animeDuration);
+        //doorRight?.DORotate(new Vector3(transform.rotation.x, transform.rotation.y + test2, transform.rotation.z), animeDuration);
+
+        doorLeft?.DORotate(new Vector3(transform.rotation.x, transform.rotation.y + (doorLeft.transform.rotation.y - 90), transform.rotation.z), animeDuration);
+        doorRight?.DORotate(new Vector3(transform.rotation.x, transform.rotation.y + (doorRight.transform.rotation.y + 90), transform.rotation.z), animeDuration);
     }
 }
